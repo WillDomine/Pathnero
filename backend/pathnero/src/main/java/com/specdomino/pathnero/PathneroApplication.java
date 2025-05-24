682891df1e47e043e87d93c14c1a2d1bbb24ff60
SpectualDomino <willdomine1@outlook.com>
2025-05-24 13:35:25 -0500

    init

---
+++ backend/pathnero/src/main/java/com/specdomino/pathnero/PathneroApplication.java
@@ -0,0 +1,13 @@
+package com.specdomino.pathnero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathneroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathneroApplication.class, args);
+	}
+
+}
---
+++ backend/pathnero/src/test/java/com/specdomino/pathnero/PathneroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.specdomino.pathnero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathneroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
